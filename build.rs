f82b21abf9352ed7de73098e87a849d7ad68aeff
Billsthename22 <emioluwagbajabiamila@gmail.com>
2026-09-12 22:41:40 +0100

    Fix Windows build for macOS framework linking

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-lib=framework=CoreLocation");
+    #[cfg(target_os = "macos")]
+    {
+        println!("cargo:rustc-link-lib=framework=CoreLocation");
+    }
 }
